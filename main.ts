87eac1edc42a77a7d83c86f6b5771d27db164070
AhmedQamar7 <mahmedkinga301@gmail.com>
2024-02-12 10:26:48 +0500

    Word counter

---
+++ main.ts
@@ -0,0 +1,12 @@
+import inquirer from "inquirer";
+
+const { sentence } = await inquirer.prompt([
+  {
+    name: "sentence",
+    type: "input",
+    message: "Enter a sentence to count the words: ",
+  },
+]);
+
+const words = sentence.trim().split(/\s+/); 
+console.log(`Your sentence word count is ${words.length}`);
